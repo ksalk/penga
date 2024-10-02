8a3fd9d57b0ce64b8740890e3f07bbbc25e0b921
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-10-03 00:31:32 +0100

    Archive old UI and create a new one to mess with built-in SWA auth

--- src/penga-ui/next.config.mjs
+++ src/penga-ui/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true
-    },
-    basePath: ""
-};
+const nextConfig = {};
 
 export default nextConfig;
