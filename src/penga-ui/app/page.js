ea62701834ace6846f004384fb90e7bdecafb226
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-10-04 00:08:32 +0200

    Add buttons for login and logout

--- src/penga-ui/app/page.js
+++ src/penga-ui/app/page.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [ userInfo, setUserInfo ] = useState();
+  const [userInfo, setUserInfo] = useState();
 
   async function getUserInfo() {
     const response = await fetch('/.auth/me');
@@ -11,27 +11,34 @@ export default function Home() {
     const { clientPrincipal } = payload;
     return clientPrincipal;
   }
-  
+
   useEffect(() => {
     const fetchUserInfo = async () => {
       setUserInfo(await getUserInfo())
     };
-    
+
     fetchUserInfo();
   }, []);
 
   return (
     <div>
-    <a href="/.auth/login/aad"><button>Login</button></a>
-    <a href="/login"><button>Login 2</button></a>
-        { userInfo && (
-            <div>
-              <div>Welcome</div>
-              <div>{ userInfo && userInfo.userDetails}</div>
-            </div>
-          )
-        }
+      {!userInfo && (
+        <div>
+          <a href="/.auth/login/aad"><button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md">Login</button></a>
+        </div>
+      )
+      }
+      {userInfo && (
+        <div>
+          <div>Welcome</div>
+          <div>{userInfo && userInfo.userDetails}</div>
+          <div>
+            <a href="/.auth/logout"><button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md">Logout</button></a>
+          </div>
+        </div>
+      )
+      }
     </div>
-    
+
   );
 }
