851adcc5eb855c3a6c4d0d62c3f47361f3935893
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-10-05 20:53:55 +0200

    Restore custom EntraId auth

--- src/penga-ui/app/page.js
+++ src/penga-ui/app/page.js
@@ -1,44 +1,51 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import { useAuth } from "./auth";
+import { useUsersApi } from "./api/userApi";
+import { useState } from "react"
 
 export default function Home() {
-  const [userInfo, setUserInfo] = useState();
+  const auth = useAuth();
+  const usersApi = useUsersApi();
+  const [account, setAccount] = useState();
 
-  async function getUserInfo() {
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
+  async function login() {
+    await auth.msalInstance.initialize();
+    await auth.msalInstance.loginPopup();
+
+    const myAccounts = auth.msalInstance.getAllAccounts();
+    auth.account = myAccounts[0];
+
+    setAccount(myAccounts[0]);
+    const response = await auth.msalInstance.acquireTokenSilent({
+      account: auth.account,
+      scopes: ['api://' + process.env.NEXT_PUBLIC_ENTRA_ID_CLIENT_ID + '/access_api']
+    });
+
+    auth.token = response.accessToken;
+    console.log(auth.account);
+    console.log(response);
+    await usersApi.saveUserInfo()
   }
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      setUserInfo(await getUserInfo())
-    };
-
-    fetchUserInfo();
-  }, []);
 
   return (
     <div>
-      {!userInfo && (
+      {!account && (
         <div>
-          <a href="/.auth/login/aad"><button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md">Login</button></a>
+          <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md" onClick={login}>Login</button>
         </div>
       )
       }
-      {userInfo && (
+      {account && (
         <div>
           <div>Welcome</div>
-          <div>{userInfo && userInfo.userDetails}</div>
+          <div>{ account.name }</div>
           <div>
-            <a href="/.auth/logout"><button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md">Logout</button></a>
+            <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md">Logout</button>
           </div>
         </div>
       )
       }
     </div>
-
   );
 }
