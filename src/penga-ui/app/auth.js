0de265672b6ddcd08d619df0c8163e8b91cb027d
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-10-10 23:45:27 +0200

    Store auth token in local storage

--- src/penga-ui/app/auth.js
+++ src/penga-ui/app/auth.js
@@ -1,6 +1,10 @@
 'use client'
 
+import { useState, useEffect } from "react";
+
 const { PublicClientApplication } = require("@azure/msal-browser")
+
+const LOCAL_STORAGE_ACCESS_TOKEN_KEY = "penga-access-token";
 
 const config = {
     auth: {
@@ -13,12 +17,44 @@ const config = {
     }
 };
 
-const data = {
-    account: null,
-    msalInstance: new PublicClientApplication(config),
-    token: ""
-};
+// For now I'm just storing access token in localStorage
+// If I find better approach I'll change it in the future
+export const useAuth2 = () => {
+    const [authData, setAuthData] = useState(null);
 
-export function useAuth() {
-    return data;
-}
+    const msalInstance = new PublicClientApplication(config);
+    msalInstance.initialize();
+
+    useEffect(() => {
+        if(!authData) {
+            setAuthData(JSON.parse(localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)));
+        }
+    }, []);
+
+    const login = async () => {
+        await msalInstance.loginPopup();
+
+        const myAccounts = msalInstance.getAllAccounts();
+    
+        const response = await msalInstance.acquireTokenSilent({
+          account: myAccounts[0],
+          scopes: ['api://' + process.env.NEXT_PUBLIC_ENTRA_ID_CLIENT_ID + '/access_api']
+        });
+    
+        const loggedInAuthData = {
+            accessToken: response.accessToken,
+            toketExpiresAt: response.expiresOn,
+            username: response.account.username
+        };
+        setAuthData(loggedInAuthData);
+        localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, JSON.stringify(loggedInAuthData));
+    };
+  
+    const logout = async () => {
+        await msalInstance.logoutPopup();
+        setAuthData(null);
+        localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+    };
+  
+    return { authData, login, logout };
+  };
--- src/penga-ui/app/page.js
+++ src/penga-ui/app/page.js
@@ -2,62 +2,24 @@
 
 import { useAuth } from "./auth";
 import { useUsersApi } from "./api/userApi";
-import { useState, useEffect } from "react";
+import { useAuth } from "./auth";
 
 export default function Home() {
-  const auth = useAuth();
+  const { authData, login, logout } = useAuth();
   const usersApi = useUsersApi();
-  const [account, setAccount] = useState();
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      await auth.msalInstance.initialize();
-      const myAccounts = auth.msalInstance.getAllAccounts();
-      setAccount(myAccounts[0]);
-    }
-    
-    fetchUserInfo();
-  }, []);
-
-  async function login() {
-    await auth.msalInstance.loginPopup();
-
-    const myAccounts = auth.msalInstance.getAllAccounts();
-    auth.account = myAccounts[0];
-
-    setAccount(myAccounts[0]);
-    const response = await auth.msalInstance.acquireTokenSilent({
-      account: auth.account,
-      scopes: ['api://' + process.env.NEXT_PUBLIC_ENTRA_ID_CLIENT_ID + '/access_api']
-    });
-
-    auth.token = response.accessToken;
-    console.log(auth.account);
-    console.log(response);
-    await usersApi.saveUserInfo();
-  }
-
-  async function logout() {
-    await auth.msalInstance.logoutPopup({
-      account: account,
-      postLogoutRedirectUri: window.location.href,
-      mainWindowRedirectUri: window.location.href
-    });
-    console.log("Logged out...");
-  }
 
   return (
     <div>
-      {!account && (
+      {!authData && (
         <div>
           <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md" onClick={login}>Login</button>
         </div>
         )
       }
-      {account && (
+      {authData && (
         <div>
           <div>Welcome</div>
-          <div>{ account.name }</div>
+          <div>{ authData.username }</div>
           <div>
             <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md" onClick={logout}>Logout</button>
           </div>
